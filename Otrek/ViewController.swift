3f7947fcbcb90b4b3f8ea61d7d3b48cd4c29bd93
Mark Boleigha <donhoenix@gmail.com>
2019-09-22 20:54:39 +0100

    Initial Commit

---
+++ Otrek/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Otrek
+//
+//  Created by Sprinthub on 22/09/2019.
+//  Copyright © 2019 Sprinthub Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
